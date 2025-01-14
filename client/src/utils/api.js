06bf59b501725b52f45ac3e634d7e05e88a1eb0e
IamSurya007 <20bq1a05j3@vvit.net>
2025-01-14 19:09:54 +0530

    Added people suggested section

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-    // baseURL: "https://echo-wave.vercel.app/",
-    baseURL: "http://localhost:5000/"
+    baseURL: "https://echo-wave.vercel.app/",
+    // baseURL: "http://localhost:5000/"
 })
